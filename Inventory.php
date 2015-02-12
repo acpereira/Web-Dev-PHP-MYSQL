3d0074503e72cf53fd1208d4915e40f707c56dbf
Adrienne Pereira <pereiraa@onid.oregonstate.edu>
2015-02-11 23:06:49 -0500

    3 hours setting up the main page and the add procedure.

---
+++ Inventory.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Video Inventory</title>
+</head>
+<body>
+	<form action="http://web.engr.oregonstate.edu/~pereiraa/store.php" method="post">
+		<div>
+			Name: <input type = "text" name ="videoName"><br>
+			Category: <input type = "text" name ="videoCat"><br>
+			Length: <input type = "text" name ="videoLength"><br>
+		</div>
+
+		<div class="button">
+		<button type="submit">Add Video</button>
+		</div>
+	</form>
+</body>
+</html>
