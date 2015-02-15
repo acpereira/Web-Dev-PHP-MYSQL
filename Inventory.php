33811d47a45426bca30dbe50ecc8fd24924067be
Adrienne Pereira <pereiraa@onid.oregonstate.edu>
2015-02-15 11:16:40 -0500

    Updated num field to make sure number validation took place.

--- Inventory.php
+++ Inventory.php
@@ -1,13 +1,13 @@
 <html>
 <head>
-	<title>Video Inventory</title>
+	<title>Add a Video</title>
 </head>
 <body>
 	<form action="http://web.engr.oregonstate.edu/~pereiraa/store.php" method="post">
 		<div>
 			Name: <input type = "text" name ="videoName"><br>
 			Category: <input type = "text" name ="videoCat"><br>
-			Length: <input type = "text" name ="videoLength"><br>
+			Length: <input type = "number" name ="videoLength" min = "0"><br>
 		</div>
 
 		<div class="button">
