06097aabc726ec11daf4262fe643796faec84c5c
Adrienne Pereira <pereiraa@onid.oregonstate.edu>
2015-02-15 11:17:00 -0500

    Database connection info

--- connect.php
+++ connect.php
@@ -2,8 +2,6 @@
 ini_set('display_errors', 'On');
 include 'storedInfo.php';
 
-
-
 $mysqli = new mysqli("oniddb.cws.oregonstate.edu","pereiraa-db",$myPassword,"pereiraa-db");
 if(!$mysqli || $mysqli->connect_errno) {
 	echo "Connection error " .$mysqli->connect_errno." ".$mysqli->connect_error;
